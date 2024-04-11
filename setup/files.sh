7dfb7f166ff86c6fdebeb1d451926d4fdfd3f821
pabl0n <73798198+pablon@users.noreply.github.com>
2024-04-11 12:09:34 +0200

    added prepwork for junegunn/vim-plug

--- setup/files.sh
+++ setup/files.sh
@@ -11,4 +11,7 @@ for file in ${FILE_LIST} ; do
   install -B ".$(date +%Y%m%d.%H%M%S)" -b  -C -v -m 600 -S "${FILES_DIR}/${file}" "${HOME}"
 done
 
+mkdir -p "${HOME}/.vim/autoload/plugged"
+curl -fLo "${HOME}/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 echo "✅ Done"
