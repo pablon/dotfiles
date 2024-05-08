6efc82d72531c5bd4ebc18c3ea9ea90c9d94e38e
pabl0n <73798198+pablon@users.noreply.github.com>
2024-05-08 11:30:17 +0200

    chore: added iterm2, fixed taps

--- setup/brew.sh
+++ setup/brew.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env bash
 ##########################################################
-# Description: Installs homebrew and all required tools.
+# Description: Installs homebrew and required tools.
 # Author: https://github.com/pablon
 ##########################################################
-# Requirements: curl
+# Requires: curl
 ##########################################################
 
-BREW_TAPS="warrensbox/tap"
+BREW_TAPS="warrensbox/tap" # tfswitch
 BREW_CASKS="homebrew/cask-fonts/font-powerline-symbols homebrew/cask-fonts/font-meslo-for-powerline"
 
-BREW_COMMON="act ansible bash bash-completion bat checkmake colordiff coreutils cowsay dos2unix drawio ffmpeg figlet findutils fontconfig fortune fzf gawk git gitui gnu-sed grep htop ipcalc jq keycastr lynx md5sha1sum netcat pngcheck pre-commit python3 ripgrep sizeup socat tmux tree vagrant watch wget yamllint yq"
+BREW_COMMON="act ansible bash bash-completion bat checkmake colordiff coreutils cowsay dos2unix drawio ffmpeg figlet findutils fontconfig fortune fzf gawk git gitui gnu-sed grep htop ipcalc iterm2 jq keycastr lynx md5sha1sum netcat pngcheck pre-commit python3 ripgrep sizeup socat tmux tree vagrant watch wget yamllint yq"
 BREW_AWS="awscli s3cmd"
 BREW_DOCKER="docker-buildx docker-completion docker-compose docker-slim hadolint"
 BREW_KUBERNETES="kubernetes-cli helm k9s kompose krew kube-linter kubecolor kubeconform kubie minikube helm-docs stern cmctl"
-BREW_TERRAFORM="terraform terraform-docs terrascan tflint tfsec infracost warrensbox/tap/tfswitch"
+BREW_TERRAFORM="terraform terraform-docs terrascan tflint tfsec infracost" # warrensbox/tap/tfswitch
 
 # Colors:
 BOLD="\033[1;37m"
-RED="\033[1;31m"
 GREEN="\033[1;92m"
 YELLOW="\033[1;93m"
 BLUE="\033[1;94m"
-MAGENTA="\033[1;95m"
 CYAN="\033[1;96m"
 NONE="\033[0m"
 
@@ -34,34 +32,43 @@ function _banner() {
 
 #===============================================================
 function _do_pyton() {
-  brew install python3
+  if ( ! command -v python3 &>/dev/null ) ; then
+    brew install python3
+  fi
 }
 #===============================================================
 function _do_bashtop() {
-  _banner "bashtop"
-  _do_pyton
-  # this is required for https://github.com/aristocratos/bashtop
-  pip3 install psutil --break-system-packages
-  brew install bash coreutils gnu-sed git
-  # Outputs current CPU temperature for OSX  # watch -d 'osx-cpu-temp -cgCf'
-  brew install osx-cpu-temp
-  local WORKDIR="${HOME}/.bashtop"
-  [ -d "${WORKDIR}" ] || mkdir -p -m 700 "${WORKDIR}"
-  (
-    cd "${WORKDIR}" && \
-    git clone https://github.com/aristocratos/bashtop.git "${WORKDIR}" && \
-    cd "${WORKDIR}" && sudo make install
-  )
+  if ( ! command -v bashtop &>/dev/null ) ; then
+    _banner "bashtop"
+    _do_pyton
+    # this is required for https://github.com/aristocratos/bashtop
+    pip3 install psutil --break-system-packages
+    # Outputs current CPU temperature for OSX  # watch -d 'osx-cpu-temp -cgCf'
+    brew install osx-cpu-temp
+    local WORKDIR="${HOME}/.bashtop"
+    # shellcheck disable=SC2174
+    [ -d "${WORKDIR}" ] || mkdir -p -m 700 "${WORKDIR}"
+    (
+      cd "${WORKDIR}" && \
+      git clone https://github.com/aristocratos/bashtop.git "${WORKDIR}" && \
+      cd "${WORKDIR}" && sudo make install
+    )
+  fi
 }
 #===============================================================
 function _do_extras() {
   _banner "extras"
 
+  # https://github.com/ryanoasis/nerd-fonts
+  brew tap homebrew/cask-fonts && brew install font-hack-nerd-font
+
   # libpq: Postgres C API library
   brew install libpq && brew link --force libpq
 
   # gh -- github cli (https://cli.github.com/)
-  brew install gh
+  if ( ! command -v gh &>/dev/null ) ; then
+    brew install gh
+  fi
   if ( command -v code &>/dev/null ) ; then
     gh config set editor "code --wait"
   elif ( command -v vim &>/dev/null ) ; then
@@ -69,8 +76,8 @@ function _do_extras() {
   fi
   gh config set pager "less -F -X -N --use-color -DNy"
   gh config set git_protocol 'ssh'
-  gh alias set co 'pr checkout'
-  gh alias set user 'auth status'
+  gh alias set co 'pr checkout' --clobber
+  gh alias set user 'auth status' --clobber
 
   # cert-manager cli (https://cert-manager.io/docs/reference/cmctl/)
   brew install cmctl
@@ -106,8 +113,8 @@ brew analytics off
 
 #===============================================================
 # Install TAPS
-[ "${BREW_TAPS}" ] && for TAP in ${BREW_CASKS} ; do
-  _banner "tap ${TAP}"
+[ "${BREW_TAPS}" ] && for tap in ${BREW_TAPS} ; do
+  _banner "tap ${tap}"
   brew tap "${tap}"
 done
 
@@ -141,11 +148,10 @@ _do_extras
 #===============================================================
 # Cleanup
 echo -e "\n${GREEN}All done${NONE}\n"
-unset BREW_COMMON
 unset BREW_AWS
+unset BREW_CASKS
+unset BREW_COMMON
 unset BREW_DOCKER
 unset BREW_KUBERNETES
 unset BREW_TERRAFORM
-unset BREW_CASKS
-
 echo "✅ Done"
