78153c61812146d81881f24298b2d3a864e45fb9
pabl0n <73798198+pablon@users.noreply.github.com>
2024-05-23 15:59:36 +0200

    fix: PlugInstall silent command

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -60,7 +60,7 @@ fi
 mkdir -p "${HOME}/.vim/autoload/plugged"
 curl -fLo "${HOME}/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # install plugins
-vim -E -s -u "${HOME}/.vimrc" +PlugInstall -qall
+vim -E -s -u "${HOME}/.vimrc" +PlugInstall +qall
 
 echo "✅ Done"
 echo "🔄 Close and reopen your terminal to reload all new configurations"
