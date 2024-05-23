2dd7cdd4b719dce8f7f4a1cd31c04be62119c20c
pabl0n <73798198+pablon@users.noreply.github.com>
2024-05-23 15:59:07 +0200

    fix: added specific install opts for linux/macos

--- setup/files.sh
+++ setup/files.sh
@@ -7,8 +7,14 @@
 FILE_LIST=".actrc .aliases .functions .gitconfig .tmux.conf .vimrc .zshrc_custom"
 FILES_DIR="$(dirname "${0}")/../"
 
+if  [[ "$(uname -s)" == "Darwin" ]] ; then
+  INSTALL_OPTS="-b -B \".$(date +%Y%m%d.%H%M%S)\" -C -v -m 600 -S"
+elif  [[ "$(uname -s)" == "Linux" ]] ; then
+  INSTALL_OPTS="--backup --suffix=.$(date +%Y%m%d.%H%M%S) -C -v -m 600"
+fi
+
 for file in ${FILE_LIST} ; do
-  install -B ".$(date +%Y%m%d.%H%M%S)" -b  -C -v -m 600 -S "${FILES_DIR}/${file}" "${HOME}"
+  install ${INSTALL_OPTS} "${FILES_DIR}/${file}" "${HOME}"
 done
 
 mkdir -p "${HOME}/.vim/autoload/plugged"
