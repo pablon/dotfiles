d53416750e887d48131b911d260dd7aa9d843219
pabl0n <73798198+pablon@users.noreply.github.com>
2025-01-20 23:19:31 +0100

    chore(nvim): improved plugins: obsidian, render-markdown and alpha dashboard

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -3,71 +3,73 @@
 return {
   "epwalsh/obsidian.nvim",
   version = "*",
-  lazy = true,
+  lazy = false,
   ft = "markdown",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  config = function()
-    require("obsidian").setup({
-      workspaces = {
-        {
-          name = "obsidian-vault",
-          path = "~/obsidian-vault",
-        },
+  -- config = function()
+  --   require("obsidian").setup({
+  opts = {
+    workspaces = {
+      {
+        name = "obsidian-vault",
+        path = "~/obsidian-vault",
       },
-      completion = {
-        nvim_cmp = true,
-        min_chars = 2,
-      },
-      notes_subdir = "Notes",
-      new_notes_location = "Notes",
-      note_id_func = function(title)
-        return title
-      end,
-      note_frontmatter_func = function(note)
-        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+    },
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+    },
+    notes_subdir = "notes",
+    new_notes_location = "notes",
+    note_id_func = function(title)
+      return title
+    end,
+    note_frontmatter_func = function(note)
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
 
-        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-          for k, v in pairs(note.metadata) do
-            out[k] = v
-          end
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
         end
+      end
 
-        return out
-      end,
-      mappings = {},
+      return out
+    end,
+    mappings = {},
 
-      templates = {
-        subdir = "Templates",
-        date_format = "%Y-%m-%d",
-        time_format = "%H:%M",
-        tags = "",
-        substitutions = {
-          yesterday = function()
-            return os.date("%Y-%m-%d", os.time() - 86400)
-          end,
-          tomorrow = function()
-            return os.date("%Y-%m-%d", os.time() + 86400)
-          end,
-        },
+    templates = {
+      subdir = "Templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      tags = "",
+      substitutions = {
+        yesterday = function()
+          return os.date("%Y-%m-%d", os.time() - 86400)
+        end,
+        tomorrow = function()
+          return os.date("%Y-%m-%d", os.time() + 86400)
+        end,
       },
+    },
 
-      daily_notes = {
-        -- Optional, if you keep daily notes in a separate directory.
-        folder = "101-Daily",
-        -- folder = "notes/dailies",
-        -- Optional, if you want to change the date format for the ID of daily notes.
-        date_format = "%Y-%m-%d",
-        -- Optional, if you want to change the date format of the default alias of daily notes.
-        alias_format = "%B %-d, %Y",
-        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-        template = nil,
-      },
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "101-Daily",
+      -- folder = "notes/dailies",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = "daily.md",
+    },
 
-      ui = {
-        enable = false, -- using render-markdown.nvim instead
-      },
-    })
-  end,
+    ui = {
+      enable = false, -- using render-markdown.nvim instead
+    },
+    -- })
+    -- end,
+  },
 }
