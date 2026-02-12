519400cd5377249c55535f9b5feefe042094fb10
pablon <73798198+pablon@users.noreply.github.com>
2026-02-12 18:22:28 +0100

    feat(setup): improve zsh plugin mgmt
    Signed-off-by: pablon <73798198+pablon@users.noreply.github.com>

--- .zshrc
+++ .zshrc
@@ -1,18 +1,20 @@
 # shellcheck disable=SC2148
 # get zsh load time - START
 # prefer gnu-date
-[[ "$(uname)" == "Darwin" ]] && ( type gdate &>/dev/null ) && alias date='gdate'
+[[ "$(uname)" == "Darwin" ]] && (type gdate &>/dev/null) && alias date='gdate'
 _zsh_start="$(date +%s%3N)"
+
+# load zsh plugins
+for plugin in $(\ls -1 ${HOME}/.zsh/ | xargs); do
+  plugin_dir="${HOME}/.zsh/${plugin}"
+  plugin_file=""
+  plugin_file="$(find "${plugin_dir}" -maxdepth 1 -type f -name '*.plugin.zsh' 2>/dev/null | head -1)"
+  [[ -n "${plugin_file}" ]] && source "${plugin_file}"
+  unset plugin plugin_dir plugin_file
+done
 
 # load ~/.zshrc_custom
 [ -r "${HOME}/.zshrc_custom" ] && source "${HOME}/.zshrc_custom"
-
-# zsh plugins
-ZSH_PLUGINS=(zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search)
-for plugin in "${ZSH_PLUGINS[@]}" ; do
-    [ -f "${HOME}/.zsh/${plugin}/${plugin}.zsh" ] &&
-    source ${HOME}/.zsh/${plugin}/${plugin}.zsh
-done
 
 # fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
@@ -29,11 +31,12 @@ eval "$(starship init zsh)"
 (type direnv &>/dev/null) && eval "$(direnv hook zsh)"
 
 cookie
-if [ -z "${TMUX}" ] ; then
-  tmux ls 2>/dev/null | while read session ; do _info "tmux session:${NONE} ${session}" ; done ; echo
+if [ -z "${TMUX}" ]; then
+  tmux ls 2>/dev/null | while read session; do _info "tmux session:${NONE} ${session}"; done
+  echo
 fi
 
 # get zsh load time - END
 _zsh_end="$(date +%s%3N)"
-printf " %.3f s\\n" "$(( ${_zsh_end} - ${_zsh_start} ))e-3"
+printf " %.3f s\\n" "$((${_zsh_end} - ${_zsh_start}))e-3"
 unset _zsh_start _zsh_end
--- setup/5_zsh.sh
+++ setup/5_zsh.sh
@@ -4,9 +4,24 @@
 # Author: https://github.com/pablon
 ##########################################################
 
-ZSH_PLUGINS=(zsh-completions zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search)
+# https://github.com/marlonrichert/zsh-autocomplete.git
+ZSH_PLUGINS=(
+	'marlonrichert/zsh-autocomplete'
+	'zsh-users/zsh-autosuggestions'
+	'zsh-users/zsh-completions'
+	'zsh-users/zsh-history-substring-search'
+	'zsh-users/zsh-syntax-highlighting'
+	'MichaelAquilina/zsh-you-should-use'
+)
 
 source "$(dirname "${0}")/.functions" || exit 1
+
+if ! command -v zsh >/dev/null 2>&1; then
+	install_pkg_${OS} zsh &>/dev/null || {
+		_error "zsh is not installed. Setup scripts failed to install it."
+		exit 1
+	}
+fi
 
 _info "Changing default shell to ${CYAN}zsh"
 sudo chsh -s $(command -v zsh) "$(whoami)" && _success "Done"
@@ -15,13 +30,19 @@ sudo chsh -s $(command -v zsh) "$(whoami)" && _success "Done"
 [ -d "${HOME}/.zsh" ] || mkdir -p "${HOME}/.zsh"
 
 for plugin in "${ZSH_PLUGINS[@]}"; do
-	if [ ! -f "${HOME}/.zsh/${plugin}/${plugin}.zsh" ]; then
-		git clone https://github.com/zsh-users/${plugin} ${HOME}/.zsh/${plugin} 2>/dev/null ||
-			exit 1
+	plugin_name="$(basename "${plugin}")"
+	if [ ! -d "${HOME}/.zsh/${plugin_name}" ]; then
+		# git clone https://github.com/zsh-users/${plugin} ${HOME}/.zsh/${plugin} 2>/dev/null ||
+		_info "Cloning zsh plugin: ${plugin_name} (https://github.com/${plugin}.git)"
+		(
+			cd "${HOME}/.zsh/" && git clone https://github.com/${plugin}.git 2>/dev/null
+			# echo "cd ${HOME}/.zsh/ && git clone https://github.com/${plugin}.git"
+		)
 	else
-		_info "Updating zsh plugin ${YELLOW}${plugin}"
-		(cd "${HOME}/.zsh/${plugin}/" && git pull --rebase)
+		_info "Updating zsh plugin ${YELLOW}${plugin_name}"
+		(cd "${HOME}/.zsh/${plugin_name}/" && git pull --rebase)
 	fi
+	unset plugin plugin_name
 done
 
 # Install iTerm2 Shell Integration
