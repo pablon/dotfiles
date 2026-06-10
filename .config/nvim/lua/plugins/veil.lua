834b7fbf74b36a291d90bdbb429e6aef49f43633
pablon <73798198+pablon@users.noreply.github.com>
2026-06-10 18:43:43 +0200

    chore(config): tweak prompt and secret tooling
    - Add API key redaction coverage to the Veil plugin config.
    - Disable Starship git metrics while keeping the settings nearby.
    - Add pnpm to the asdf package list for JavaScript tooling.
    - Add gnupg to the Homebrew package list for signing support.
    
    Signed-off-by: pablon <73798198+pablon@users.noreply.github.com>

--- .config/nvim/lua/plugins/veil.lua
+++ .config/nvim/lua/plugins/veil.lua
@@ -10,6 +10,8 @@ return {
     extra_patterns = {
       -- *TOKEN* (case-sensitive): TOKEN anywhere in the key name
       { pattern = "([%w_]*TOKEN[%w_]*%s*[=:]%s*[\"']?)([^\"'\n]+)", group = 2 },
+      -- *api*key (case-insensitive)
+      { pattern = "([%w_]*[Aa][Pp][Ii][%w_]*[Kk][Ee][Yy]%s*[=:]%s*[\"']?)([^\"'\n]+)", group = 2 },
       -- *access*key (case-insensitive)
       { pattern = "([%w_]*[Aa][Cc][Cc][Ee][Ss][Ss][%w_]*[Kk][Ee][Yy]%s*[=:]%s*[\"']?)([^\"'\n]+)", group = 2 },
       -- *secret*key (case-insensitive)
